ff3e3b2f6c2dd98dd3fb13ed4571ba30d43c652d
Chris <hereokay88@gmail.com>
2023-11-09 11:54:57 +0900

    user로 엔트리 변경 및 소숫점 2자리

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ function GasCostChecker() {
   const [error, setError] = useState(null);
 
   const checkGasCost = async () => {
+    const startTime = performance.now(); // 시간 측정 시작
     setIsLoading(true);
     setError(null);
     try {
-      // 백엔드 엔드포인트로 요청을 보냅니다.
-      const response = await axios.get(`http://localhost:8080/api/etherscan/onlyGasUSDT?address=${address}`);
-      console.log(response.data); // 전체 응답 구조 확인
-      console.log(typeof response.data.spendGasUSDT); // spendGasUSDT 필드의 타입 확인
-      setSpendGasUSDT(response.data); // 응답 구조에 따라 접근해야 할 프로퍼티를 확인하세요.
+      const response = await axios.get(`http://localhost:8080/api/etherscan/user?address=${address}`);
+      const roundedSpendGasUSDT = parseFloat(response.data.spendGasUSDT.toFixed(2));
+      setSpendGasUSDT(roundedSpendGasUSDT);
     } catch (error) {
       setError('트랜잭션 주소를 찾을 수 없거나 오류가 발생했습니다.');
-      setSpendGasUSDT(null); // 오류 발생 시 spendGasUSDT 초기화
+      setSpendGasUSDT(null);
+    } finally {
+      setIsLoading(false);
+      const endTime = performance.now(); // 시간 측정 종료
+      console.log(`처리 시간: ${endTime - startTime} 밀리초`); // 콘솔에 시간 차이 출력
     }
-    setIsLoading(false);
   };
 
   return (
@@ -28,7 +30,7 @@ function GasCostChecker() {
       <h1>가스 비용 확인</h1>
       <input
         type="text"
-        placeholder="트랜잭션 주소 입력"
+        placeholder="계정 주소 입력"
         value={address}
         onChange={(e) => setAddress(e.target.value)}
       />
